ea68d8bca15cfdcfb2416f6dffaaf9849a04e5fd
indraprasid <joaneindra.prasetyawan@gmail.com>
2025-02-12 11:57:10 +0700

    add folder materi and tugas including the python files of tugas1, tugas2

---
+++ tugas/tugas2/tugas2-joaneindra.py
@@ -0,0 +1,16 @@
+# Loop dari 1 sampai 30
+angka = 1
+while angka < 30:
+    angka += 1
+    # Cek kelipatan 3 dan 5, maka cetak kata dangdut
+    if angka % 3 == 0 and angka % 5 == 0:
+        print("dangdut")
+    # Cek kelipatan 3, maka cetak kata dang
+    elif angka % 3 == 0:
+        print("dang")
+    # Cek kelipatan 5, maka cetak kata dut
+    elif angka % 5 == 0:
+        print("dut")
+    # Cetak angka biasa
+    else:
+        print(angka)
